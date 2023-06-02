3706536e69108799ad5eeab60b91b0e77e3ff0d0
Cachilox <marianopvp1@gmail.com>
2023-06-02 16:47:24 -0300

    datos

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function laCajaDePandora(numero) {
-    //Aqui va el código
+  //Aqui va el código
+  if (typeof numero !== "number") {
+    throw new Error("El parámetro debe ser un número");
+  }
+
+  if (isNaN(numero)) {
+    throw new Error("El parámetro debe ser un número válido");
+  }
+
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
 }
+
+const mariano = () => {
+  return {
+    nombre: "Mariano",
+    apellido: "Alvarez",
+    edad: 20,
+    nacionalidad: "Argentina",
+  };
+};
