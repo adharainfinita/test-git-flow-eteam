86114245e12a8b120c5572d87ead6cf7c1b4affc
Adhara <adharanosalevich@gmail.com>
2023-06-02 16:38:52 -0300

    my_solution

---
+++ index.js
@@ -0,0 +1,22 @@
+function laCajaDePandora(numero){
+    if(numero / 2){
+        let binary = "";
+        while(numero > 0){
+            let digit = numero % 2;
+            numero = Math.floor(numero / 2);
+            binary = digit + binary;
+        }
+        return binary;
+    }
+    return numero.toString(16)
+    }
+
+function myName ({name, age, nationality}){
+    return {
+        name,
+        age,
+        nationality
+    }
+}
+
+myName("Adhara", 25, "argentinian");
