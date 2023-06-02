4fdb78e3b8146b5c7ae9afc3f23f251e52be2f08
Cachilox <marianopvp1@gmail.com>
2023-06-02 17:12:24 -0300

    fix funcion

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ function laCajaDePandora(numero) {
     throw new Error("El parámetro debe ser un número");
   }
 
-  if (isNaN(numero)) {
-    throw new Error("El parámetro debe ser un número válido");
-  }
-
   if (numero % 2 === 0) {
     return numero.toString(2);
   } else {
